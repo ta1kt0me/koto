7dd0f4196b8208ffb7472bd99cde2c9788a9fb15
ta1kt0me <p.wadachi@gmail.com>
2016-01-30 17:06:45 +0900

    Mix do & mi & ra

--- sine.rb
+++ sine.rb
@@ -2,18 +2,31 @@ require "coreaudio"
 
 dev = CoreAudio.default_output_device
 buf = dev.output_buffer(1024)
-STANDARD_PITCH = 400
+STANDARD_PITCH = 440
 BIT_DEPTH = 0x25A0
 PLAY_TIME = 12
 
 th = Thread.start do
-  wave = [3, 5, 7, 8, 10, 12, 14, 15].map { |i|
+  # wave = [3, 5, 7, 8, 10, 12, 14, 15].map { |i|
+  wave_a = [0].map { |i|
     phase = Math::PI * 2.0 * STANDARD_PITCH * 2 ** (i/12.0) / dev.nominal_rate
     (0...dev.nominal_rate).map { |j|
       Math.sin(phase*j) * BIT_DEPTH
     }
   }.flatten
-
+  wave_e = [7].map { |i|
+    phase = Math::PI * 2.0 * STANDARD_PITCH * 2 ** (i/12.0) / dev.nominal_rate
+    (0...dev.nominal_rate).map { |j|
+      Math.sin(phase*j) * BIT_DEPTH
+    }
+  }.flatten
+  wave_c = [3].map { |i|
+    phase = Math::PI * 2.0 * STANDARD_PITCH * 2 ** (i/12.0) / dev.nominal_rate
+    (0...dev.nominal_rate).map { |j|
+      Math.sin(phase*j) * BIT_DEPTH
+    }
+  }.flatten
+  wave = wave_a.zip(wave_e, wave_c).map { |arr| arr.inject :+ }
   buf << wave
   # see sine wave
   wave.each do |w|
