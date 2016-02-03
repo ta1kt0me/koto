e2f1a9303057059e5acca4519626439cceea9eca
ta1kt0me <p.wadachi@gmail.com>
2016-02-03 23:15:20 +0900

    Use inject

--- nokogiri_graph.rb
+++ nokogiri_graph.rb
@@ -4,12 +4,6 @@ require 'matrix'
 require 'tkextlib/tcllib/plotchart'
 include Tk::Tcllib::Plotchart
 
-STANDARD_PITCH = 440.0
-BIT_DEPTH = 0x05A0
-PLAY_TIME = 2
-
-tone = 12
-waves = []
 # (1..50).each do |i|
 #   phase = STANDARD_PITCH * 2 * Math::PI / CoreAudio.default_output_device.nominal_rate
 #   waves << (0...PLAY_TIME*CoreAudio.default_output_device.nominal_rate).map { |j|
@@ -18,26 +12,28 @@ waves = []
 #   }
 # end
 # data_3 = waves[0].zip(*waves[1..-1]).map { |e| e.inject(:+) }
-position_in_period = 0.0
-position_in_period_delta = STANDARD_PITCH * 2 ** (tone/12.0) / CoreAudio.default_output_device.nominal_rate
-(0...PLAY_TIME*CoreAudio.default_output_device.nominal_rate).each do |i|
+# position = 0.0
+STANDARD_PITCH = 440.0
+BIT_DEPTH = 0x05A0
+PLAY_TIME = 2
+tone = 12
+waves = []
+phase = STANDARD_PITCH * 2 ** (tone/12.0) / CoreAudio.default_output_device.nominal_rate
+(0...PLAY_TIME * CoreAudio.default_output_device.nominal_rate).inject(0.0) do |position, i|
   # sine wave
-  waves << Math.sin(position_in_period * 2 * Math::PI) * BIT_DEPTH
+  # waves << Math.sin(position * 2 * Math::PI) * BIT_DEPTH
   # sawtooth
-  # waves << ((position_in_period * 2.0) - 1.0) * BIT_DEPTH
+  # waves << ((position * 2.0) - 1.0) * BIT_DEPTH
   # squre
-  # waves << ((position_in_period >= 0.5) ? BIT_DEPTH : -BIT_DEPTH)
+  # waves << ((position >= 0.5) ? BIT_DEPTH : -BIT_DEPTH)
   # triangle
-  # waves << BIT_DEPTH - (((position_in_period * 2.0) - 1.0) * BIT_DEPTH * 2.0).abs
+  waves << BIT_DEPTH - (((position * 2.0) - 1.0) * BIT_DEPTH * 2.0).abs
   # white noise
   # waves << rand(-BIT_DEPTH..BIT_DEPTH)
-  position_in_period += position_in_period_delta
-  if position_in_period >= 1.0
-    position_in_period -= 1.0
-  end
+  phase * i - (phase * i).floor
 end
 
-LEN = 500
+LEN = 250
 # m = Matrix[*data_3]
 column_zero = (0..LEN).to_a
 column_one = waves[0..LEN]
